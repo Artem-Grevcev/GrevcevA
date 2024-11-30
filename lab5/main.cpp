e54caf71579dd632a4410c42f5fedf76c8c5a90e
Artem-Grevcev <artemon454111@gmail.com>
2024-11-30 12:09:32 +0200

    Add files via upload

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -1,85 +1,20 @@
-//ЗАДАНИЕ 1
 #include <iostream>
-
+#include <vector>
 int main() {
-    int A, B, C;
-    
-    std::cout << "Enter A: ";
-    std::cin >> A;
-    
-    std::cout << "Enter B: ";
-    std::cin >> B;
-    
-    std::cout << "Enter C: ";
-    std::cin >> C;
-    
-    if (A % B == 0) {
-        if (B > C) {
-            std::cout << A/B+C;
-        } else if (B < C) {
-            std::cout << A/B-C;
-        } else {
-            std::cout << (A+B)*C;
-        }
-    } else {
-        std::cout << (A+B)*C;
-    }
-    
-    return 0;
-}
-//ЗАДАНИЕ 2
-#include <iostream>
-
-int main() {
-    int N;
-    
-    std::cout << "Enter a number (1-7): ";
+    int N=0;
+    std::cout << "Введите натуральное число N: ";
     std::cin >> N;
-    
-    switch (N) {
-        case 1:
-            std::cout << "Monday";
-            break;
-        case 2:
-            std::cout << "Tuesday";
-            break;
-        case 3:
-            std::cout << "Wednesday";
-            break;
-        case 4:
-            std::cout << "Thursday";
-            break;
-        case 5:
-            std::cout << "Friday";
-            break;
-        case 6:
-            std::cout << "Saturday";
-            break;
-        case 7:
-            std::cout << "Sunday";
-            break;
-        default:
-            std::cout << "Error: Invalid input";
-    }
-    
-    return 0;
-}
-//ЗАДАНИЕ 3
-#include <iostream>
 
-int main() {
-    int x;
-    
-    std::cout << "Enter a number (-1 or 1): ";
-    std::cin >> x;
-    
-    if (x == -1) {
-        std::cout << "Negative number";
-    } else if (x == 1) {
-        std::cout << "Positive number";
-    } else {
-        std::cout << "Error: Invalid input";
+    int product = 1;
+    while (N > 1 || N<1000000000) {
+        int digit = N % 10;
+        if (digit % 2 == 0) {
+            product *= digit;
+        }
+        N /= 10;
     }
-    
+
+    std::cout << "Произведение чётных цифр числа N: " << product << std::endl;
+
     return 0;
 }
